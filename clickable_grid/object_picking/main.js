7fb0c7e3383b2695a681a51c5558da7f46f5aa53
Matt Zhao <mattzhao@Matts-MacBook-Pro.local>
2014-01-11 20:50:20 -0600

    added an example of a grid I can click on

---
+++ clickable_grid/object_picking/main.js
@@ -0,0 +1,146 @@
+window.onload = function() {
+	
+	var container = document.getElementById( 'container' ),
+		containerWidth, containerHeight,
+		renderer,
+		scene,
+		camera,
+		cubes,
+		geom,
+		range = 50,
+		size = 10,
+		mouseVector,
+		axes,
+		controls;
+
+	containerWidth = container.clientWidth;
+	containerHeight = container.clientHeight;
+
+	// Set up renderer, scene and camera
+	renderer = new THREE.CanvasRenderer();
+	renderer.setSize( containerWidth, containerHeight );
+	container.appendChild( renderer.domElement );
+
+	renderer.setClearColorHex( 0xeeeedd, 1.0 );
+
+	scene = new THREE.Scene();
+
+	camera = new THREE.PerspectiveCamera( 45, containerWidth / containerHeight, 1, 10000 );
+	camera.position.set( 0, 0, range * 2 );
+	camera.lookAt( new THREE.Vector3( 0, 0, 0 ) );
+
+	// Add some cubes to the scene
+	geom = new THREE.PlaneGeometry( 10, 10);
+
+	cubes = new THREE.Object3D();
+	scene.add( cubes );
+
+	var i = -50,
+		j = -50;
+
+	while (i <= 50) {
+		j = -50;
+		while (j <= 50) {
+			var grayness = Math.random() * 0.5 + 0.25,
+			mat = new THREE.MeshBasicMaterial(),
+			cube = new THREE.Mesh( geom, mat );
+			mat.color.setRGB( grayness, grayness, grayness );
+			cube.position.set( i, j, 0 );
+			//cube.rotation.set( Math.random(), Math.random(), Math.random() ).multiplyScalar( 2 * Math.PI );
+			cube.grayness = grayness;
+			cubes.add( cube );
+			j += 10;
+		}
+		i += 10;
+	}
+
+	// Axes
+	axes = buildAxes();
+	scene.add( axes );
+
+	// Picking stuff
+
+	projector = new THREE.Projector();
+	mouseVector = new THREE.Vector3();
+
+	// User interaction
+	window.addEventListener( 'mousemove', onMouseMove, false );
+	window.addEventListener( 'resize', onWindowResize, false );
+
+	controls = new THREE.TrackballControls( camera );
+	controls.zoomSpeed = 0.1;
+
+	// And go!
+	animate();
+
+
+	function onMouseMove( e ) {
+		
+		mouseVector.x = 2 * (e.clientX / containerWidth) - 1;
+		mouseVector.y = 1 - 2 * ( e.clientY / containerHeight );
+
+		var raycaster = projector.pickingRay( mouseVector.clone(), camera ),
+			intersects = raycaster.intersectObjects( cubes.children );
+
+		cubes.children.forEach(function( cube ) {
+			cube.material.color.setRGB( cube.grayness, cube.grayness, cube.grayness );
+		});
+
+			
+		for( var i = 0; i < intersects.length; i++ ) {
+			var intersection = intersects[ i ],
+				obj = intersection.object;
+
+			obj.material.color.setRGB( 1.0 - i / intersects.length, 0, 0 );
+		}
+		
+	}
+
+	function onWindowResize( e ) {
+		containerWidth = container.clientWidth;
+		containerHeight = container.clientHeight;
+		renderer.setSize( containerWidth, containerHeight );
+		camera.aspect = containerWidth / containerHeight;
+		camera.updateProjectionMatrix();
+	}
+
+	function animate() {
+		requestAnimationFrame( animate );
+		controls.update();
+		renderer.render( scene, camera );
+	}
+
+
+	function buildAxes() {
+		var axes = new THREE.Object3D();
+
+		axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 100, 0, 0 ), 0xFF0000, false ) ); // +X
+		axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( -100, 0, 0 ), 0x800000, true) ); // -X
+		axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 100, 0 ), 0x00FF00, false ) ); // +Y
+		axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, -100, 0 ), 0x008000, true ) ); // -Y
+		axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, 100 ), 0x0000FF, false ) ); // +Z
+		axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, -100 ), 0x000080, true ) ); // -Z
+
+		return axes;
+
+	}
+
+	function buildAxis( src, dst, colorHex, dashed ) {
+		var geom = new THREE.Geometry(),
+			mat; 
+
+		if(dashed) {
+			mat = new THREE.LineDashedMaterial({ linewidth: 1, color: colorHex, dashSize: 5, gapSize: 5 });
+		} else {
+			mat = new THREE.LineBasicMaterial({ linewidth: 1, color: colorHex });
+		}
+
+		geom.vertices.push( src.clone() );
+		geom.vertices.push( dst.clone() );
+
+		var axis = new THREE.Line( geom, mat );
+
+		return axis;
+
+	}
+}
