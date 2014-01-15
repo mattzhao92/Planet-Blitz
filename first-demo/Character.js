de3894aec314c9435eb3668acab8397423b019be
Peter Chang <psc2@rice.edu>
2014-01-15 14:47:37 -0600

    Extract Character, Game, and Grid classes

---
+++ first-demo/Character.js
@@ -0,0 +1,74 @@
+var Character = Class.extend({
+// Class constructor
+    init: function (args) {
+        'use strict';
+        // Set the different geometries composing the humanoid
+        var head = new THREE.SphereGeometry(32, 8, 8),
+            // Set the material, the "skin"
+        material = new THREE.MeshLambertMaterial(args);
+        // Set the character modelisation object
+        this.mesh = new THREE.Object3D();
+        this.mesh.position.y = 48;
+        // Set and add its head
+        this.head = new THREE.Mesh(head, material);
+        this.head.position.y = 0;
+        this.mesh.add(this.head);
+        // Set the vector of the current motion
+        this.direction = new THREE.Vector3(0, 0, 0);
+        // Set the current animation step
+        this.step = 0;
+    },
+    // Update the direction of the current motion
+    setDirection: function (controls) {
+        'use strict';
+        // Either left or right, and either up or down (no jump or dive (on the Y axis), so far ...)
+        var x = controls.left ? 1 : controls.right ? -1 : 0,
+            y = 0,
+            z = controls.up ? 1 : controls.down ? -1 : 0;
+        this.direction.set(x, y, z);
+    },
+    // Process the character motions
+    motion: function () {
+        'use strict';
+        // (if any)
+        if (this.direction.x !== 0 || this.direction.z !== 0) {
+            // Rotate the character
+            this.rotate();
+            // And, only if we're not colliding with an obstacle or a wall ...
+            if (this.collide()) {
+                return false;
+            }
+            // ... we move the character
+            this.move();
+            return true;
+        }
+    },
+    // Rotate the character
+    rotate: function () {
+        'use strict';
+        // Set the direction's angle, and the difference between it and our Object3D's current rotation
+
+        var angle = Math.atan2(this.direction.x, this.direction.z),
+        difference = angle - this.mesh.rotation.y;
+
+        // Now if we haven't reached our target angle
+        if (difference !== 0) {
+            // We slightly get closer to it
+            this.mesh.rotation.y += difference / 4;
+        }
+    },
+    move: function () {
+        'use strict';
+        // We update our Object3D's position from our "direction"
+        this.mesh.position.x += this.direction.x * ((this.direction.z === 0) ? 4 : Math.sqrt(8));
+        this.mesh.position.z += this.direction.z * ((this.direction.x === 0) ? 4 : Math.sqrt(8));
+        // Now let's use Sine and Cosine curves, using our "step" property ...
+        this.step += 1 / 4;
+    },
+    collide: function () {
+        'use strict';
+        // INSERT SOME MAGIC HERE
+        return false;
+    }
+});
+
---
+++ first-demo/Grid.js
@@ -0,0 +1,172 @@
+/* 
+ * Grid
+ * constructor input args : {d_x, d_y, square_size}
+ */
+    var Grid = Class.extend({
+        // Class constructor
+        init: function (width, length, squareSize) {
+            'use strict';
+            // Set the "world" modelisation object
+            this.cubes = new THREE.Object3D();
+            this.characters = new THREE.Object3D();
+
+            this.planeGeometry = new THREE.PlaneGeometry(width, length);
+
+            this.numOfCharacters = 1;
+            this.users = [];
+
+            this.squareSize = squareSize;
+
+            for (var i = 0; i < this.numOfCharacters; i++) {
+                this.user = new Character({
+                     color: 0x7A43B6
+                });
+                this.users.push(this.user);
+                this.characters.add(this.user.mesh);
+            }
+
+            this.drawGridSquares(this.squareSize);
+        },
+
+        drawGridSquares: function(size) {
+            var geom = new THREE.PlaneGeometry(size, size);
+
+            var planeMaterial = new THREE.MeshLambertMaterial({color: 0xffffff});
+
+            this.numberSquaresOnXAxis = this.planeGeometry.width/size;
+            this.numberSquaresOnZAxis = this.planeGeometry.height/size;
+
+            for (var j = 0 ; j < this.numberSquaresOnZAxis; j++) {
+                for (var i = 0 ; i < this.numberSquaresOnXAxis; i++) {
+
+                    var grayness = Math.random() * 0.5 + 0.25;
+                    var mat = new THREE.MeshBasicMaterial({overdraw: true});
+                    var cube = new THREE.Mesh( geom, mat );
+                            
+                    mat.color.setRGB( grayness, grayness, grayness );
+                    cube.grayness = grayness;
+
+                    cube.position.x =- ((this.planeGeometry.width)/2)+2+(i*size);
+                    cube.position.y = 1;
+                    cube.position.z =- ((this.planeGeometry.height)/2)+2+(j*size);
+                    cube.rotation.x = -0.5 * Math.PI;
+
+                    console.log("cube x : " + cube.position.x + " cube z : " + cube.position.z);
+
+                    this.cubes.add(cube);
+                }
+            }
+        },
+
+        getNumberSquaresOnXAxis: function () {
+            return this.numberSquaresOnXAxis;
+        },
+
+        getNumberSquaresOnZAxis: function () {
+            return this.numberSquaresOnZAxis;
+        },
+
+
+        hasObstacleAtCell: function(args) {
+
+        },
+
+        hasCharacterAtCell: function(args) {
+
+        },
+
+        isCellOutOfBounds: function(args) {
+
+        },
+
+        motion: function(args) {
+            // move the selected character to new location
+            if (this.selectedCharacter) {
+                this.selectedCharacter = null;
+                this.selectedCharacter.motion();
+                this.selectedCharacter = null;
+            }
+        },
+
+         // Event handlers
+        setControls: function () {
+            'use strict';
+            // Within jQuery's methods, we won't be able to access "this"
+            var user = this.selectedCharacter;
+            var controls = {
+                    left: false,
+                    up: false,
+                    right: false,
+                    down: false
+                };
+            // When the user presses a key 
+            jQuery(document).keydown(function (e) {
+                console.log("keydown");
+                var prevent = true;
+                // Update the state of the attached control to "true"
+                switch (e.keyCode) {
+                    case 37:
+                    controls.left = true;
+                    break;
+                    case 38:
+                    controls.up = true;
+                    break;
+                    case 39:
+                    controls.right = true;
+                    break;
+                    case 40:
+                    controls.down = true;
+                    break;
+                    default:
+                    prevent = false;
+                }
+                // Avoid the browser to react unexpectedly
+                if (prevent) {
+                    e.preventDefault();
+                } else {
+                    return;
+                }
+                // Update the character's direction
+                if (user) {
+                    user.setDirection(controls);
+                }
+            });
+            // When the user releases a key
+            jQuery(document).keyup(function (e) {
+                var prevent = true;
+                // Update the state of the attached control to "false"
+                switch (e.keyCode) {
+                    case 37:
+                    controls.left = false;
+                    break;
+                    case 38:
+                    controls.up = false;
+                    break;
+                    case 39:
+                    controls.right = false;
+                    break;
+                    case 40:
+                    controls.down = false;
+                    break;
+                    default:
+                    prevent = false;
+                }
+                // Avoid the browser to react unexpectedly
+                if (prevent) {
+                    e.preventDefault();
+                } else {
+                    return;
+                }
+                // Update the character's direction
+                if (user) {
+                    user.setDirection(controls);
+                }
+            });
+            // On resize
+            jQuery(window).resize(function () {
+                // Redefine the size of the renderer
+                basicScene.setAspect();
+            });
+        },
+
+    });
