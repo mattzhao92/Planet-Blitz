8023cd49891143893631d1c2f855a8330426d0f2
Peter Chang <psc2@rice.edu>
2014-01-26 14:48:47 -0600

    Remove old code

--- first-demo/Grid.js
+++ first-demo/Grid.js
@@ -2,258 +2,260 @@
  * Grid
  * constructor input args : {d_x, d_y, square_size}
  */
-    var Grid = Class.extend({
-        // Class constructor
-        init: function (width, length, squareSize, scene, camera) {
-            'use strict';
+var Grid = Class.extend({
+    // Class constructor
+    init: function(width, length, squareSize, scene, camera) {
+        'use strict';
 
-            this.gridWidth = width;
-            this.gridLength = length;
+        this.gridWidth = width;
+        this.gridLength = length;
 
-            this.scene = scene;
-            this.camera = camera;
+        this.scene = scene;
+        this.camera = camera;
 
-            // Set the "world" modelisation object
-            this.tiles = new THREE.Object3D();
-            this.drawGridSquares(width, length, squareSize);
+        // Set the "world" modelisation object
+        this.tiles = new THREE.Object3D();
+        this.drawGridSquares(width, length, squareSize);
 
-            this.characters = new THREE.Object3D();
-            this.numOfCharacters = 3;
-            this.charactersOnMap = [];
-            
-            this.squareSize = squareSize;
+        this.characters = new THREE.Object3D();
+        this.numOfCharacters = 3;
+        this.charactersOnMap = [];
 
-            this.characterFactory = new CharacterFactory();
+        this.squareSize = squareSize;
 
-            var scope = this;
-            for (var i = 0; i < this.numOfCharacters; i++) {
+        this.characterFactory = new CharacterFactory();
 
-                var charArgs = {
-                    color: 0x7A43B6, 
-                    position : {x : -((width)/2)+2+((i+3)*this.squareSize), 
-                    y : 5, 
-                    world: scope}};
+        var scope = this;
+        for (var i = 0; i < this.numOfCharacters; i++) {
 
-                var character = this.characterFactory.createCharacter(charArgs);
-                this.charactersOnMap.push(character);
-                
-                // console.log("added character " + character.mesh);
-                this.scene.add(character);
+            var charArgs = {
+                position: {
+                    x: -((width) / 2) + 2 + ((i + 3) * this.squareSize),
+                    y: 5,
+                },
+                world: scope
+            };
+
+            var character = this.characterFactory.createCharacter(charArgs);
+            this.charactersOnMap.push(character);
+
+            // console.log("added character " + character.mesh);
+            this.scene.add(character);
+        }
+
+        this.characterBeingSelected = null;
+
+        this.setControls();
+        this.setupMouseMoveListener();
+    },
+
+    markCharacterAsSelected: function(character) {
+        // deselect previous character if there was one
+        if (this.characterBeingSelected) {
+            this.characterBeingSelected.deselect();
+        }
+
+        this.characterBeingSelected = character;
+
+        // show character movement speed
+    },
+
+    deselectAll: function() {
+        this.charactersOnMap.forEach(function(character) {
+            character.deselect();
+        });
+    },
+
+    setupMouseMoveListener: function() {
+        var scope = this;
+
+        window.addEventListener('mousemove',
+            function(event) {
+
+                var projector = new THREE.Projector();
+                var mouseVector = new THREE.Vector3();
+
+                mouseVector.x = 2 * (event.clientX / window.innerWidth) - 1;
+                mouseVector.y = 1 - 2 * (event.clientY / window.innerHeight);
+
+                var raycaster = projector.pickingRay(mouseVector.clone(), scope.camera),
+                    intersects = raycaster.intersectObjects(scope.tiles.children);
+
+                scope.tiles.children.forEach(function(tile) {
+                    tile.material.color.setRGB(tile.grayness, tile.grayness, tile.grayness);
+                });
+
+                for (var i = 0; i < intersects.length; i++) {
+                    var intersection = intersects[i],
+                        obj = intersection.object;
+
+                    obj.onMouseOver();
+                }
+            }, false);
+    },
+
+    getWorldPosition: function() {
+        // get world position of a grid
+
+    },
+
+    drawGridSquares: function(width, length, size) {
+        this.tileFactory = new TileFactory(size);
+
+        this.numberSquaresOnXAxis = width / size;
+        this.numberSquaresOnZAxis = length / size;
+
+        for (var j = 0; j < this.numberSquaresOnXAxis; j++) {
+            for (var i = 0; i < this.numberSquaresOnZAxis; i++) {
+                var tile = this.tileFactory.createTile(i, j);
+
+                tile.position.x = -((width) / 2) + (i * size);
+                tile.position.y = 0;
+                tile.position.z = -((length) / 2) + (j * size);
+                tile.rotation.x = -0.5 * Math.PI;
+
+                this.tiles.add(tile);
             }
+        }
 
-            this.characterBeingSelected = null;
+        this.scene.add(this.tiles);
+    },
 
-            this.setControls();
-            this.setupMouseMoveListener();
-        },
-        
-        markCharacterAsSelected: function(character) {
-            // deselect previous character if there was one
-            if (this.characterBeingSelected) {
-                this.characterBeingSelected.deselect();
-            }
+    getNumberSquaresOnXAxis: function() {
+        return this.numberSquaresOnXAxis;
+    },
 
-            this.characterBeingSelected = character;
-
-            // show character movement speed
-        },
-
-        deselectAll: function() {
-            this.charactersOnMap.forEach(function (character) {
-                character.deselect();
-            });
-        },
-
-        setupMouseMoveListener: function() {
-            var scope = this;
-
-            window.addEventListener( 'mousemove', 
-                function(event) {
-
-                    var projector = new THREE.Projector();
-                    var mouseVector = new THREE.Vector3();
-        
-                    mouseVector.x = 2 * (event.clientX / window.innerWidth) - 1;
-                    mouseVector.y = 1 - 2 * ( event.clientY / window.innerHeight );
-
-                    var raycaster = projector.pickingRay( mouseVector.clone(), scope.camera ),
-                        intersects = raycaster.intersectObjects( scope.tiles.children );
-
-                    scope.tiles.children.forEach(function( tile ) {
-                        tile.material.color.setRGB( tile.grayness, tile.grayness, tile.grayness );
-                    });
-                    
-                    for( var i = 0; i < intersects.length; i++ ) {
-                        var intersection = intersects[ i ],
-                            obj = intersection.object;
-
-                        obj.onMouseOver();
-                    }
-                }, false );
-        },
-
-        getWorldPosition: function() {
-            // get world position of a grid
-
-        },
-
-        drawGridSquares: function(width, length, size) {
-            this.tileFactory = new TileFactory(size);
-
-            this.numberSquaresOnXAxis = width/size;
-            this.numberSquaresOnZAxis = length/size;
-
-            for (var j = 0 ; j < this.numberSquaresOnXAxis; j++) {
-              for (var i = 0 ; i < this.numberSquaresOnZAxis; i++) {
-                    var tile = this.tileFactory.createTile(i, j);
-
-                    tile.position.x =- ((width)/2) + (i * size);
-                    tile.position.y = 0;
-                    tile.position.z =- ((length)/2) + (j * size);
-                    tile.rotation.x = -0.5 * Math.PI;
-
-                    this.tiles.add(tile);
-              }
-            }
-
-            this.scene.add(this.tiles);
-        },
-
-        getNumberSquaresOnXAxis: function () {
-            return this.numberSquaresOnXAxis;
-        },
-
-        getNumberSquaresOnZAxis: function () {
-            return this.numberSquaresOnZAxis;
-        },
+    getNumberSquaresOnZAxis: function() {
+        return this.numberSquaresOnZAxis;
+    },
 
 
-        hasObstacleAtCell: function(args) {
+    hasObstacleAtCell: function(args) {
 
-        },
+    },
 
-        hasCharacterAtCell: function(args) {
+    hasCharacterAtCell: function(args) {
 
-        },
+    },
 
-        isCellOutOfBounds: function(args) {
+    isCellOutOfBounds: function(args) {
 
-        },
+    },
 
-        motion: function(args) {
-            for (var i = 0; i < this.charactersOnMap.length; i++) {
-                var character = this.charactersOnMap[i];
-                character.dequeueMotion();
-            }
-        },
+    motion: function(args) {
+        for (var i = 0; i < this.charactersOnMap.length; i++) {
+            var character = this.charactersOnMap[i];
+            character.dequeueMotion();
+        }
+    },
 
-         // Event handlers
-        setControls: function () {
-            'use strict';
-            // Within jQuery's methods, we won't be able to access "this"
-            var scope = this;
-            var user = this.characterBeingSelected;
-            var controls = {
-                    left: false,
-                    up: false,
-                    right: false,
-                    down: false
-                };
-            // When the user presses a key 
-            jQuery(document).keydown(function (e) {
-                var prevent = true;
-                // Update the state of the attached control to "true"
-                switch (e.keyCode) {
-                    case 37:
+    // Event handlers
+    setControls: function() {
+        'use strict';
+        // Within jQuery's methods, we won't be able to access "this"
+        var scope = this;
+        var user = this.characterBeingSelected;
+        var controls = {
+            left: false,
+            up: false,
+            right: false,
+            down: false
+        };
+        // When the user presses a key 
+        jQuery(document).keydown(function(e) {
+            var prevent = true;
+            // Update the state of the attached control to "true"
+            switch (e.keyCode) {
+                case 37:
                     controls.right = true;
                     break;
-                    case 38:
+                case 38:
                     controls.down = true;
                     break;
-                    case 39:
+                case 39:
                     controls.left = true;
                     break;
-                    case 40:
+                case 40:
                     controls.up = true;
                     break;
-                    default:
+                default:
                     prevent = false;
-                }
-                // Avoid the browser to react unexpectedly
-                if (prevent) {
-                    e.preventDefault();
-                } else {
-                    return;
-                }
-                // Update the character's direction
-                if (scope.characterBeingSelected) {
-                    console.log("enqueueMotion ---------- ");
-                    scope.characterBeingSelected.enqueueMotion();
-                    scope.characterBeingSelected.setDirection(controls);
-                } else {
-                    console.log("BAD BAD BAD ");
-                }
-            });
-            // When the user releases a key
-            jQuery(document).keyup(function (e) {
-                var prevent = true;
-                // Update the state of the attached control to "false"
-                switch (e.keyCode) {
-                    case 37:
+            }
+            // Avoid the browser to react unexpectedly
+            if (prevent) {
+                e.preventDefault();
+            } else {
+                return;
+            }
+            // Update the character's direction
+            if (scope.characterBeingSelected) {
+                console.log("enqueueMotion ---------- ");
+                scope.characterBeingSelected.enqueueMotion();
+                scope.characterBeingSelected.setDirection(controls);
+            } else {
+                console.log("BAD BAD BAD ");
+            }
+        });
+        // When the user releases a key
+        jQuery(document).keyup(function(e) {
+            var prevent = true;
+            // Update the state of the attached control to "false"
+            switch (e.keyCode) {
+                case 37:
                     controls.right = false;
                     break;
-                    case 38:
+                case 38:
                     controls.down = false;
                     break;
-                    case 39:
+                case 39:
                     controls.left = false;
                     break;
-                    case 40:
+                case 40:
                     controls.up = false;
                     break;
-                    default:
+                default:
                     prevent = false;
+            }
+            // Avoid the browser to react unexpectedly
+            if (prevent) {
+                e.preventDefault();
+            } else {
+                return;
+            }
+            // Update the character's direction
+            if (user) {
+                user.setDirection(controls);
+            }
+        });
+        // On resize
+        jQuery(window).resize(function() {
+            // Redefine the size of the renderer
+            //basicScene.setAspect();
+        });
+
+        var scope = this;
+        window.addEventListener('mousedown',
+            function(event) {
+
+                var projector = new THREE.Projector();
+                var mouseVector = new THREE.Vector3();
+
+                mouseVector.x = 2 * (event.clientX / window.innerWidth) - 1;
+                mouseVector.y = 1 - 2 * (event.clientY / window.innerHeight);
+
+                var raycaster = projector.pickingRay(mouseVector.clone(), scope.camera);
+
+                // recursively call intersects
+                var intersects = raycaster.intersectObjects(scope.charactersOnMap, true);
+
+                console.log("intersect length is  " + intersects.length + "  " + scope.charactersOnMap);
+                if (intersects.length > 0) {
+                    var firstIntersect = intersects[0];
+                    firstIntersect.object.onSelect(scope);
                 }
-                // Avoid the browser to react unexpectedly
-                if (prevent) {
-                    e.preventDefault();
-                } else {
-                    return;
-                }
-                // Update the character's direction
-                if (user) {
-                    user.setDirection(controls);
-                }
-            });
-            // On resize
-            jQuery(window).resize(function () {
-                // Redefine the size of the renderer
-                //basicScene.setAspect();
-            });
 
-               var scope = this;
-                window.addEventListener( 'mousedown', 
-                    function(event) {
-
-                        var projector = new THREE.Projector();
-                        var mouseVector = new THREE.Vector3();
-            
-                        mouseVector.x = 2 * (event.clientX / window.innerWidth) - 1;
-                        mouseVector.y = 1 - 2 * ( event.clientY / window.innerHeight );
-
-                        var raycaster = projector.pickingRay( mouseVector.clone(), scope.camera );
-
-                        // recursively call intersects
-                        var intersects = raycaster.intersectObjects(scope.charactersOnMap, true);
-
-                        console.log("intersect length is  "+ intersects.length+ "  "+ scope.charactersOnMap);
-                        if (intersects.length > 0) {
-                            var firstIntersect = intersects[0];
-                            firstIntersect.object.onSelect(scope);
-                        }
-
-                    }, false );
-            },
+            }, false);
+    },
 
 
-    });
+});
