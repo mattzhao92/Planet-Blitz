9a85ddcfc15ad65e13dad7241e3365b59dbb8630
wenxuancai <wenxuancai1992@gmail.com>
2014-02-03 19:11:59 -0600

    retab

--- gameserver.js
+++ gameserver.js
@@ -14,17 +14,17 @@ app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
 app.set('view options', {layout : false});
 app.configure(function() {
-	app.use(express.static(__dirname + '/server/public'));
-	app.use(express.static(__dirname + '/server/views'));
-	app.use(express.static(__dirname + '/first-demo'));
-	app.use(express.static(__dirname + '/libs'));
-	app.use(express.static(__dirname + '/css'));
-	app.use(express.static(__dirname + '/assets'));
+  app.use(express.static(__dirname + '/server/public'));
+  app.use(express.static(__dirname + '/server/views'));
+  app.use(express.static(__dirname + '/first-demo'));
+  app.use(express.static(__dirname + '/libs'));
+  app.use(express.static(__dirname + '/css'));
+  app.use(express.static(__dirname + '/assets'));
 });
 
 // Server index page.
 app.get('/', function(req, res) {
-	res.render('index.jade');
+  res.render('index.jade');
 });
 server.listen(8080);
 
@@ -35,31 +35,31 @@ var Message = netconst.Message;
 var Move = netconst.Move;
 // IO communication.
 io.sockets.on('connection', function(socket) {
-	socket.set('pseudo', 'player' + numPlayers);
-	numPlayers++;
-	console.log('Player connection, #' + numPlayers);
+  socket.set('pseudo', 'player' + numPlayers);
+  numPlayers++;
+  console.log('Player connection, #' + numPlayers);
 
-	if (numPlayers == 2) {
-		gameStart = true;
-	}
+  if (numPlayers == 2) {
+    gameStart = true;
+  }
 
-	if (gameStart) {
-		socket.on(Message.MOVE, function(message) {
-			socket.get('pseudo', function(error, name) {
-				console.log('user ' + name + ' send this : ' + message);
-				// Forward it to other player.
-				socket.broadcast.emit(Message.MOVE, message);
-			});
-		});
+  if (gameStart) {
+    socket.on(Message.MOVE, function(message) {
+      socket.get('pseudo', function(error, name) {
+        console.log('user ' + name + ' send this : ' + message);
+        // Forward it to other player.
+        socket.broadcast.emit(Message.MOVE, message);
+      });
+    });
 
-		socket.on(Message.SHOOT, function(message) {
-			socket.get('pseudo', function(error, name) {
-				console.log('shot msg');
-				console.log('user ' + name + ' send this : ' + message);
-				// Forward it to other player.
-				socket.broadcast.emit(Message.SHOOT, message);
-			});
-		});
+    socket.on(Message.SHOOT, function(message) {
+      socket.get('pseudo', function(error, name) {
+        console.log('shot msg');
+        console.log('user ' + name + ' send this : ' + message);
+        // Forward it to other player.
+        socket.broadcast.emit(Message.SHOOT, message);
+      });
+    });
 
-	}
+  }
 });
