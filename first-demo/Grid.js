ef02bd6d4a60f367ef6900e460b47d298398be7f
wenxuancai <wenxuancai1992@gmail.com>
2014-02-08 11:09:54 -0600

    Add sever multiple game deplex

--- first-demo/Grid.js
+++ first-demo/Grid.js
@@ -73,7 +73,7 @@ var Grid = Class.extend({
             // deselect character
             character.deselect();
 
-            // deselect tiles
+            // deselect tiles.
             this.highlightedTiles.forEach(function(tile) {
                 tile.reset();
             });
