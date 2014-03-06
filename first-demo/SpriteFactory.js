b62bde2170b1eaa7196e6ad51a96a1cc4d6db863
Peter Chang <psc2@rice.edu>
2014-03-05 18:17:57 -0600

    Added add sprite cmd and remove sprite cmd, which both work - used for post-init and pre-destroy behavior

--- first-demo/SpriteFactory.js
+++ first-demo/SpriteFactory.js
@@ -1,14 +1,68 @@
+// controls initialization of all sprites
+// also the place to access all sprite lists
 var SpriteFactory = Class.extend({
 
 	init: function(sceneAddCmd, sceneRemoveCmd) {
 		this.sceneAddCmd = sceneAddCmd;
-		this.sceneRemove = sceneRemoveCmd;
+		this.sceneRemoveCmd = sceneRemoveCmd;
+
+		this.robots = [];
+	},
+
+	removeRobot: function(robot) {
+		var index = this.robots.indexOf(robot);
+
+		if (index > -1) {
+			this.robots.splice(index, -1);
+		}
 	},
 
 	createRobot: function(world, team, characterSize) {
-		var character = new Character(this.sceneAddCmd, this.sceneRemoveCmd, world, team, characterSize);
-		character.addSelf();
-		return character;
+		var scope = this;
+
+		// decorator - allow character to add itself to its container
+		var postInitCmd = new SpriteCmd(function(sprite) {
+			scope.sceneAddCmd.execute(sprite);
+			scope.robots.push(sprite);
+		});
+
+		// decorator - allow character to remove itself form the container
+		var postDestroyCmd = new SpriteCmd(function(sprite) {
+			scope.sceneRemoveCmd.execute(sprite);
+			scope.removeRobot(sprite);
+		});
+
+		var robot = new Character(postInitCmd, postDestroyCmd, this, world, team, characterSize);
+
+		// start post initialization hook
+		robot.addSelf();
+
+		return robot;
+	},
+
+	createAmmoBar: function(characterSize, position, barAspectRatio) {
+		var ammoBar = new AmmoBar(this.sceneAddCmd, this.sceneRemoveCmd, characterSize, position, barAspectRatio);
+		ammoBar.addSelf();
+
+		return ammoBar;
+	},
+
+	createHealthBar: function(characterSize, position, barAspectRatio, maximumHealth) {
+		var healthBar = new HealthBar(this.sceneAddCmd, this.sceneRemoveCmd, characterSize, position, barAspectRatio, maximumHealth);
+		healthBar.addSelf();
+
+		return healthBar;
+	},
+
+	createCooldownBar: function(characterSize, position, barAspectRatio, coolDownCount) {
+		var cooldownBar = new CooldownBar(this.sceneAddCmd, this.sceneRemoveCmd, characterSize, position, 10, coolDownCount);
+		cooldownBar.addSelf();
+
+		return cooldownBar;
+	},
+
+	getCharacters: function() {
+		return this.robots;
 	}
 
 });
