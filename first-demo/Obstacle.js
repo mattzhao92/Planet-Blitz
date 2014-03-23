49654611738d8975e0749a51552b5e0a01cccd65
xelliotpenguin <zhenghuan.zhao@gmail.com>
2014-03-23 11:12:05 -0500

    obstacles all shown as cubes

--- first-demo/Obstacle.js
+++ first-demo/Obstacle.js
@@ -26,23 +26,24 @@ var Obstacle = Class.extend({
     	var obstacle_bounding_cube = new THREE.CubeGeometry(size,size,size);
 		var obstacle_bounding_cube_material = new THREE.MeshLambertMaterial({opacity: this.opacity, transparent:true});
 
-		this.box_mesh = new THREE.Mesh( obstacle_bounding_cube, obstacle_bounding_cube_material );
+        var combinedMaterials = new THREE.MeshNormalMaterial();
+        this.box_mesh = new THREE.Mesh(new THREE.CubeGeometry(40, 40, 40), combinedMaterials)
 
-		var path_to_mesh = "";
+		// var path_to_mesh = "";
 		
-		switch(this.obstacleType)
-		{
-		case "rock":
-		  path_to_mesh = "blendermodels/rock.js";
-		  break;
-		case "powerup-health":
-		  path_to_mesh = "blendermodels/powerup-health.js"
-		  break;
-		default:
-		  path_to_mesh = "blendermodels/rock.js";
-		  break;
-		}
-        this.loadFile(path_to_mesh, this.box_mesh);
+		// switch(this.obstacleType)
+		// {
+		// case "rock":
+		//   path_to_mesh = "blendermodels/rock.js";
+		//   break;
+		// case "powerup-health":
+		//   path_to_mesh = "blendermodels/powerup-health.js"
+		//   break;
+		// default:
+		//   path_to_mesh = "blendermodels/rock.js";
+		//   break;
+		// }
+  //       this.loadFile(path_to_mesh, this.box_mesh);
     },
 
     setXPos: function(xPos) {
@@ -78,27 +79,41 @@ var Obstacle = Class.extend({
 		var fullFilename = filename;
 		var myloader = new THREE.JSONLoader();
 		var scope = this;
-		myloader.load(fullFilename, function(geometry, materials) {	     
-            // materials[0].color = scope.teamColor;
-            // materials[1].color = scope.teamColor;
-            // materials[10].color = scope.teamColor;
 
-            var combinedMaterials = new THREE.MeshFaceMaterial(materials);
-            scope.obstacle_mesh = new THREE.Mesh(geometry, combinedMaterials);
 
-            // scale to correct width / height / depth
-            geometry.computeBoundingBox();
+        var combinedMaterials = new THREE.MeshNormalMaterial();
+        scope.obstacle_mesh = new THREE.Mesh(new THREE.CubeGeometry(40, 40, 40), combinedMaterials);
+        var size = Constants.ORIGINAL_TILESIZE;
+        scope.obstacle_mesh.scale.set(size/10, size/10, size/10);
+        
+        box_mesh.add(scope.obstacle_mesh);
 
-            // TODO: should use this bounding box to compute correct scale
-            var boundingBox = geometry.boundingBox;
-            var width = geometry.boundingBox.max.x - geometry.boundingBox.min.x;
-            var height = geometry.boundingBox.max.y - geometry.boundingBox.min.y;
-            var depth = geometry.boundingBox.max.z - geometry.boundingBox.min.z;
-            var size = Constants.ORIGINAL_TILESIZE;
-            scope.obstacle_mesh.scale.set(size/10, size/10, size/10);
+		// myloader.load(fullFilename, function(geometry, materials) {
+  //           // var combinedMaterials;
+  //           // if (fullFilename == "blendermodels/rock.js") {
+  //           //     combinedMaterials = new THREE.MeshNormalMaterial();
+  //           //     scope.obstacle_mesh = new THREE.Mesh(new THREE.CubeGeometry(40, 40, 40), combinedMaterials);
+  //           // } else {
+  //           //     combinedMaterials = new THREE.MeshFaceMaterial(materials);
+  //           //     scope.obstacle_mesh = new THREE.Mesh(geometry, combinedMaterials);
+  //           // }
+
+  //           var ombinedMaterials = new THREE.MeshNormalMaterial();
+  //           scope.obstacle_mesh = new THREE.Mesh(new THREE.CubeGeometry(40, 40, 40), combinedMaterials);
+
+  //           // scale to correct width / height / depth
+  //           geometry.computeBoundingBox();
+
+  //           // TODO: should use this bounding box to compute correct scale
+  //           var boundingBox = geometry.boundingBox;
+  //           var width = geometry.boundingBox.max.x - geometry.boundingBox.min.x;
+  //           var height = geometry.boundingBox.max.y - geometry.boundingBox.min.y;
+  //           var depth = geometry.boundingBox.max.z - geometry.boundingBox.min.z;
+  //           var size = Constants.ORIGINAL_TILESIZE;
+  //           scope.obstacle_mesh.scale.set(size/10, size/10, size/10);
             
-            box_mesh.add(scope.obstacle_mesh);
-    	});
+  //           box_mesh.add(scope.obstacle_mesh);
+  //   	});
     },
 
     getMesh: function() {
