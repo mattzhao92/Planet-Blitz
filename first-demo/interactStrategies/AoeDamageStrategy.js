7347daade249c3395bc6af63f6726e2b184236d5
Peter Chang <psc2@rice.edu>
2014-04-10 21:23:09 -0500

    Added AOE damage strategy for area of effect damage

---
+++ first-demo/interactStrategies/AoeDamageStrategy.js
@@ -0,0 +1,26 @@
+var AoeDamageStrategy = InteractStrategy.extend({
+	init: function(dmgToApply) {
+		this._super();
+		this.dmgToApply = dmgToApply;
+		this.shouldApplyDamage = true;
+	},
+
+	interact: function(ctxSprite, otherSprite, dispatcher) {
+		if (this.checkOverlap(ctxSprite, otherSprite)) {
+            if (otherSprite instanceof Character) {
+			    if (otherSprite.team != ctxSprite.owner.team) {
+					ctxSprite.destroy();
+					sendHitMsg(ctxSprite, otherSprite, this.dmgToApply);			    		
+		    		this.shouldApplyDamage = false;
+			    }
+            } else if (otherSprite instanceof Obstacle) {
+                ctxSprite.destroy();
+            }
+		}
+	},
+
+	checkOverlap: function(obj1, obj2) {
+	    var combinedRadius = obj1.getRadius() + obj2.getRadius();
+	    return combinedRadius * combinedRadius >= obj1.getRepr().position.distanceToSquared(obj2.getRepr().position);
+	}
+});
